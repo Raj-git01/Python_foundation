82e116bafccc6cb35fa6d2e63e289808afef93b1
Raj <raj20063251@gmail.com>
2025-12-25 03:04:05 +0530

    pracrice

---
+++ strings.py
@@ -0,0 +1,15 @@
+name = "Jennifer's name "
+another = name
+'''
+Multi line comment
+o
+'''
+print(name[0:2])
+print(another)
+print(name[0])
+print(name[-3])
+print(name[1:-1])
+print(name[:2])
+print(name[:])
+print(name[1:])
+print(name[:-1])
