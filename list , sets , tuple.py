57c1b815f2e7e11d45f4b83111e8f72e860d081a
Raj <raj20063251@gmail.com>
2025-12-25 03:58:56 +0530

    practice

---
+++ list , sets , tuple.py
@@ -0,0 +1,13 @@
+fruits={"apple", "banana", "orange"}
+fruit=input("Search for fruit: ")
+if fruit in fruits:
+    print(f"Hey {fruit} was found")
+else:
+    print(f"SOB {fruit} was not found")
+'''
+fruits.append()
+fruits.add()
+fruits.remove()
+fruits.pop()
+dont forget to use end="" for for loop , to make line horizontal
+'''
