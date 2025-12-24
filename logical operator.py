295985e93bbe7e83775ee24dfa66cbc31d3d18d1
Raj <raj20063251@gmail.com>
2025-12-25 02:31:16 +0530

    practice

---
+++ logical operator.py
@@ -0,0 +1,12 @@
+has_good_credit=True
+has_criminal_record=False
+if has_good_credit and not has_criminal_record:
+    print("Eligible for loan")
+has_high_income=True
+has_good_credit=True
+if has_high_income and has_good_credit:
+    print('He is good')
+has_high_income=True
+has_good_credit=False
+if has_high_income or has_good_credit:
+    print("He is not bad")
